11a2923cc31a684ea146d3eb4afc999e5698c258
cacho <394369786@qq.com>
2018-02-03 17:21:50 +0800

    add Selenium folder

--- Submit.bat
+++ Submit.bat
@@ -3,11 +3,11 @@
 f:
 cd F:\SoftwareTest\Github\03 Selenium
 
-git init
-git remote add gitee https://gitee.com/Cacho/Selenium.git
-git remote add github https://github.com/Cachozeng/Selenium.git
+REM git init
+REM git remote add gitee https://gitee.com/Cacho/Selenium.git
+REM git remote add github https://github.com/Cachozeng/Selenium.git
 
-git pull gitee master
+REM git pull gitee master
 git add .
 git commit -m "add Selenium folder"
 git push gitee master
